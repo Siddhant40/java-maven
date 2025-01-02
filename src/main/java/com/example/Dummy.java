cd5afb86a1bf7775057bf3b8cf0e326d5f951855
Siddhant <97793203+Siddhant40@users.noreply.github.com>
2025-01-02 19:17:22 +0530

    Delete src/main/java/com/example/Dummy.java

--- src/main/java/com/example/Dummy.java
+++
@@ -1,9 +0,0 @@
-// File: src/main/java/com/example/Dummy.java
-package com.example;
-
-public class Dummy {
-    public static void main(String[] args) {
-        System.out.println("Hello, JaCoCo!");
-    }
-}
-
