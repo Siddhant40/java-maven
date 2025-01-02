2b2392b40748b1f3a95dfd6ae9fbc65cc1bb8796
Siddhant <97793203+Siddhant40@users.noreply.github.com>
2025-01-02 15:13:56 +0530

    Add files via upload

---
+++ src/main/java/com/example/Dummy.java
@@ -0,0 +1,9 @@
+// File: src/main/java/com/example/Dummy.java
+package com.example;
+
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("Hello, JaCoCo!");
+    }
+}
+
